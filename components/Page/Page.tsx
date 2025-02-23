213fca327800906fff2c49e9bf32c57d01abd873
thancode99382 <dat99382@donga.edu.vn>
2025-02-23 20:08:23 +0700

    scroll smooth and blur

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -94,6 +94,11 @@ const Page: React.FC<PageProps> = ({
       <Footer />
 
       <div className="gradients-container">
+      <div className="g1"></div>
+      <div className="g2"></div>
+      <div className="g3"></div>
+      <div className="g4"></div>
+      <div className="g5"></div>
         <div className="interactive" ref={interactiveRef}></div>
       </div>
     </div>
